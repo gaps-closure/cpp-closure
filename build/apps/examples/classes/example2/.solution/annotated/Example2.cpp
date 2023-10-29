f7dd15ed5944bba1ed76b9e2bb34b2a47fa6f0e0
ta-chen <tchen@peratonlabs.com>
2023-10-29 09:40:04 -0400

    reorg

---
+++ build/apps/examples/classes/example2/.solution/annotated/Example2.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <string>
+
+#include "Extra.hpp"
+// #include "annotations.hpp"
+
+template <typename T, int label>
+using annotate = T;
+
+#define ORANGE 1
+
+class Example2
+{
+private:
+  Extra extra;
+
+public:
+  // @OrangeShareable
+  annotate<int, ORANGE> myConstant;
+
+  int getValue() {
+    return this->extra.getValue();
+  }
+
+  Example2() : extra() {
+  }
+};
+
+// @OrangeMain
+int main(int argc, char **argv)
+{
+  Example2 e;
+  printf("Hello Example 1: %d\n", e.getValue());
+}
